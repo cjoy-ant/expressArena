3d5a2494d7eb5973f8d1c631b1be3e3bd76cda1a
Christine Antonio <cjoy.antonio@gmail.com>
2021-02-19 18:40:56 -0600

    pizza and burgers

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
+// This is middleware that requests pass through
+// on their way to the final handler
+app.use(morgan("dev"));
+
+//This is the final request handler
 app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
 app.listen(8000, () => {
   console.log(
-    "Express server is listening on port 8080! https://localhost:8080/"
+    "Express server is listening on port 8000! http://localhost:8000/"
   );
+});
+
+app.get("/burgers", (req, res) => {
+  res.send("We have juicy cheese burgers!");
+});
+
+app.get("/pizza/pepperoni", (req, res) => {
+  res.send("Your pizza is on the way!");
+});
+
+app.get("/pizza/pineapple", (req, res) => {
+  res.send("We don't serve that here. Never call again!");
 });
